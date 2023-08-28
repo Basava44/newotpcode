6c28ff0896affb9056bfbc4e425fb332520e72a8
Karibasaveshwara_TG <karibasaveshwara.tg@moonraft.com>
2023-08-28 12:23:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'newotp-test';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  otp = '';
+
+  ngOnInit(): void {
+    console.log('WEB OTP API');
+  }
+
+  submit() {
+    console.log('Hello World');
+    console.log(this.otp);
+  }
 }
